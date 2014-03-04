1d24c46d20eb364aa7429bc6e4b5a40a1bcd7b24
sanyuancap <sanyuancap@foxmail.com>
2014-03-04 15:50:50 -0800

    a
    a

---
+++ Assets/js0.js
@@ -0,0 +1,52 @@
+﻿
+
+//旋转移动速度
+var Speed = 50;
+
+//控制对象
+var Control;
+
+function Update()
+{
+  //得到控制对象
+  Control = GetComponent(js1); 
+
+  //判断按键 
+
+  if(Input.GetKey(KeyCode.W))
+  {
+  	    //前进
+   		Control.ForWard();
+   		
+
+  }else if(Input.GetKey(KeyCode.S))	
+  {
+		//后退
+  		Control.Back();
+  }
+
+    if(Input.GetKey(KeyCode.A))
+  {
+  	    //前左
+   		Control.GLeft();
+
+  }else if(Input.GetKey(KeyCode.D))	
+  {
+		//后右
+  		Control.GRight();
+  }
+
+  if(Input.GetKey(KeyCode.Q))
+  {
+  	//左旋转
+
+    Control.leftRotate(Vector3.up *Time.deltaTime * -Speed);
+
+  }else if(Input.GetKey(KeyCode.E))
+  {
+    //右旋转
+  	Control.RightRotate(Vector3.up *Time.deltaTime * Speed);
+
+  }
+
+}
